ec2b298c64ed9d160ebc39eb804fa22b407f8b73
Sourabh Bucha <sourabhking14@gmail.com>
2021-09-07 19:33:10 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React , {useState, useEffect} from "react";
 import './App.css';
+import { Data } from "./Data";
 
 function App() {
+  const [products,setProducts] = useState(Data);
+  const [cart,setCart] = useState([]);
+
+  useEffect(()=>{
+    console.log(cart.length)
+  })
+
+  const btn = (x) =>{
+    if(x === 0){
+      return <p>Add to Cart</p>
+    }else{
+      return <p>Already in the cart</p>
+    }
+  }
+  const disable_btn = (x) => {
+    if( x === 0) {return false;}
+    else{
+      return true;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+
+      </div>
+    <div className="content">
+      <div className="sidebar">
+
+      </div>
+      <div className="items">
+        {products.map((product) => (
+          <div className="product" key={product.id}>
+            <img src={product.img_src} alt="" />
+            <p>{product.name}</p>
+            <span>{product.price}</span>
+            <button onClick = {() => {
+              product.qty=1;
+              setCart([...cart, product]);
+            }} disabled={disable_btn(product.qty)}> {btn(product.qty)} </button>
+          </div>
+        ))} 
+      </div>
+    </div>  
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,123 @@
+export const Data = [
+    {
+        id: "1",
+        img_src: "https://m.media-amazon.com/images/I/61p683nGweL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Armless Chair CHR4002 Plastic Chair (Bright Red) by GN Furniture (6PC Combo)",
+        desc: "this is the item",
+        category: "category1",
+        Weight: "1000",
+        price: "2000",
+        qty: 0
+    },
+    {
+        id: "2",
+        img_src: "https://m.media-amazon.com/images/I/61coJ3JMC7L._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Armless Chair CHR4001",
+        desc: "this is the item",
+        category: "category1",
+        Weight: "1000",
+        price: "2500",
+        qty: 0
+    },
+    {
+        id: "3",
+        img_src: "https://m.media-amazon.com/images/I/81fQmDN1nvL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Amazon Brand - Stone & Beam Waynes Fabric Accent/Lounge Chair (Blue)",
+        desc: "this is the item",
+        category: "category2",
+        Weight: "1000",
+        price: "5500",
+        qty: 0
+    },
+    {
+        id: "4",
+        img_src: "https://m.media-amazon.com/images/I/61gl9-ez6UL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Armless Chair CHR4002 Plastic Chair (Pear Wood) by GN Furniture (4PC Combo)",
+        desc: "this is the item",
+        category: "category2",
+        Weight: "1000",
+        price: "1500",
+        qty: 0
+    },
+    {
+        id: "5",
+        img_src: "https://m.media-amazon.com/images/I/51jejNqBhyL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Armless Chair CHR4001",
+        desc: "this is the item",
+        category: "category3",
+        Weight: "1000",
+        price: "3000",
+        qty: 0
+    },
+    {
+        id: "6",
+        img_src: "https://m.media-amazon.com/images/I/41MtaHN08RL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Black Modern Cane Chair",
+        desc: "this is the item",
+        category: "category3",
+        Weight: "1000",
+        price: "4000",
+        qty: 0
+    },
+    {
+        id: "7",
+        img_src: "https://m.media-amazon.com/images/I/61iBE4Nhu0L._AC_UL480_FMwebp_QL65_.jpg",
+        name: "AVRO FURNITURE, 2581 Plastic Chairs, Set of 2 Pcs, Plastic Modern Ergonomic Chair Without Cushion, 200 kilograms (Orange), 2 Pieces",
+        desc: "this is the item",
+        category: "category4",
+        Weight: "1000",
+        price: "5000",
+        qty: 0
+    },
+    {
+        id: "8",
+        img_src: "https://m.media-amazon.com/images/I/61p683nGweL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Armless Chair",
+        desc: "this is the item",
+        category: "category1",
+        Weight: "1000",
+        price: "2000",
+        qty: 0
+    },
+    {
+        id: "9",
+        img_src: "https://m.media-amazon.com/images/I/61gl9-ez6UL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "(Pear Wood) by GN Furniture (4PC Combo)",
+        desc: "this is the item",
+        category: "category2",
+        Weight: "1000",
+        price: "1500",
+        qty: 0
+    },
+    {
+        id: "10",
+        img_src: "https://m.media-amazon.com/images/I/51jejNqBhyL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Armless Chair",
+        desc: "this is the item",
+        category: "category3",
+        Weight: "1000",
+        price: "3000",
+        qty: 0
+    },
+    {
+        id: "11",
+        img_src: "https://m.media-amazon.com/images/I/41MtaHN08RL._AC_UL480_FMwebp_QL65_.jpg",
+        name: "Cane Chair",
+        desc: "this is the item",
+        category: "category3",
+        Weight: "1000",
+        price: "4000",
+        qty: 0
+    },
+    {
+        id: "12",
+        img_src: "https://m.media-amazon.com/images/I/61iBE4Nhu0L._AC_UL480_FMwebp_QL65_.jpg",
+        name: "AVRO FURNITURE,2 Pieces",
+        desc: "this is the item",
+        category: "category4",
+        Weight: "1000",
+        price: "5000",
+        qty: 0
+    },
+    
+]
